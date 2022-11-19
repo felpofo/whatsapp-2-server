79c2627ae92ddc831fa8e46d20d18a33f54c8baf
felpofo <felipepitolpuhl@gmail.com>
2022-11-19 00:17:49 -0300

    auto setting last message id

--- src/index.ts
+++ src/index.ts
@@ -43,15 +43,31 @@ function sortUsers() {
 
 io.on("connection", (socket) => {
   socket.emit("onlineUsers", users);
-  socket.emit("previousMessages", messages);
+  socket.emit("allMessages", messages);
 
-  socket.on("setName", name => {
+  socket.on("setName", (name, previousId) => {
+    if (socket.data.name) return;
+
     socket.data.name = name;
 
     users.push({ id: socket.id, name });
     sortUsers();
 
     io.emit("onlineUsers", users);
+
+    if (previousId) {
+      messages = messages.map(message => {
+        if (message.user.id !== previousId)
+          return message;
+
+        const newMessage = message;
+        newMessage.user.id = socket.id;
+
+        return newMessage;
+      });
+
+      io.emit("allMessages", messages);
+    }
   });
 
   socket.on("message", text => {
@@ -71,7 +87,7 @@ io.on("connection", (socket) => {
 
   socket.on("deleteMessage", id => {
     messages = messages.filter(message => message.id !== id && message);
-    
+
     io.emit("deleteMessage", id);
   });
 
--- src/types.ts
+++ src/types.ts
@@ -13,12 +13,12 @@ export type Message = {
 export interface ClientToServerEvents {
   message: (message: string) => void;
   deleteMessage: (id: string) => void;
-  setName: (name: string) => void;
+  setName: (name: string, previousId?: string) => void;
 }
 
 export interface ServerToClientEvents {
   message: (message: Message) => void;
-  previousMessages: (messages: Message[]) => void;
+  allMessages: (messages: Message[]) => void;
   deleteMessage: (id: string) => void;
   onlineUsers: (users: User[]) => void;
 }
